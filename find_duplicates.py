4f2fda02d3316eb9ded7e5ff9eecf0e6ae7ef4cc
Kons <katashi.rain@gmail.com>
2021-09-05 20:15:04 +0900

    same result with lambda function and filer

--- find_duplicates.py
+++ find_duplicates.py
@@ -1,4 +1,7 @@
+# find duplicates in the list
 my_list = [1,2,3,4,4,5,6,3]
+
+# for loop approach with list attribute .count
 new_list = []
 for i in my_list:
     if my_list.count(i) > 1:
@@ -8,3 +11,7 @@ new_set = set(new_list)
 output = ', '.join(map(str, new_set))
 print(f'Duplicate numers are {output}') 
 
+# same result with lambda function and filer
+lambda_set = set(filter(lambda item: my_list.count(item) > 1, my_list))
+output = ', '.join(map(str, lambda_set))
+print(f'Duplicate numers are {output}') 
