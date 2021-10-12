e8be416276910c206b25566d6a765f3a78c1c468
Kons <katashi.rain@gmail.com>
2021-10-12 15:47:14 +0900

    update fixed

--- dict_add_update.py
+++ dict_add_update.py
@@ -5,13 +5,15 @@ dict1 = {}
 def func():
     global dict1
     for key in range(5):
-        value = randint(10, 99)
+        value = [randint(10, 99)]
+        print(value)
         # adding new
         dict1.update({key: value})
         print(dict1)
     # updating
-    dict1[1] = [dict1[1], 50]
-    dict1[1] = [dict1[1], 51]
+    dict1[1] = dict1[1] + [50]
+    dict1[1] = dict1[1] + [52]
+
 
 func()
 print(dict1)
