90f4796501912e8a8d8d1988ac3bd3376a5b99ac
Kons M <kons@SierraPro.local>
2023-03-18 22:36:38 +0900

    Added dynamic logger file path

--- logging/logging_json.py
+++ logging/logging_json.py
@@ -1,10 +1,15 @@
 import logging
 import logging.config
 import json
+from os.path import dirname, abspath
+
+BASE_DIR = dirname(dirname(abspath(__file__)))
+LOG_PATH = BASE_DIR + "/files/logger.log"
 
 # Read the configuration from JSON
-with open('./files/logging_conf.json', 'r') as json_string:
-    data = json.load(json_string)
+with open(BASE_DIR + '/files/logging_conf.json', 'r') as f:
+    data = json.load(f)
+    data["handlers"]["file"]["filename"] = LOG_PATH
     logging.config.dictConfig(data)
 
 # Retrieve the logger
